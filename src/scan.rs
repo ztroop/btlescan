c488a9f89269e27801e1de6441341057007cf1ab
Zackary Troop <zack.tro@gmail.com>
2024-03-18 23:51:54 -0400

    Add support for bluez/linux connects

--- src/scan.rs
+++ src/scan.rs
@@ -1,12 +1,15 @@
+#[cfg(target_os = "linux")]
+use bluez_async::MacAddress;
 use btleplug::api::{
     Central, CentralEvent, Manager as _, Peripheral, PeripheralProperties, ScanFilter,
 };
-use btleplug::platform::Manager;
+use btleplug::platform::{Manager, PeripheralId};
 use futures::StreamExt;
 use std::error::Error;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use tokio::sync::mpsc;
+use uuid::Uuid;
 
 use crate::structs::{Characteristic, DeviceInfo};
 
@@ -61,6 +64,43 @@ pub async fn bluetooth_scan(
 
 /// Gets the characteristics of a Bluetooth device and returns them as a `Vec<Characteristic>`.
 /// The device is identified by its address or UUID.
-pub async fn get_characteristics(_device_id: &str) -> Result<Vec<Characteristic>, Box<dyn Error>> {
-    unimplemented!();
+pub async fn get_characteristics(id: &str) -> Result<Vec<Characteristic>, Box<dyn Error>> {
+    let manager = Manager::new().await?;
+    let adapters = manager.adapters().await?;
+    let central = adapters.into_iter().next().ok_or("No adapters found")?;
+
+    let peripheral_id: Option<PeripheralId>;
+    #[cfg(target_os = "macos")]
+    {
+        let uuid_id = Uuid::parse_str(id)?;
+        peripheral_id = Some(PeripheralId::from(uuid_id));
+    }
+    #[cfg(target_os = "linux")]
+    {
+        let mac_addr = MacAddress::from_str(id)?;
+        peripheral_id = Some(PeripheralId::from(mac_addr));
+    }
+
+    // Short circuit if no peripheral is not found
+    if peripheral_id.is_none() {
+        return Err("Peripheral not found".into());
+    }
+
+    let peripheral = central.peripheral(&peripheral_id.unwrap()).await?;
+    peripheral.connect().await?;
+
+    let characteristics = peripheral.characteristics();
+    let mut result = Vec::new();
+    for characteristic in characteristics {
+        result.push(Characteristic {
+            uuid: characteristic.uuid,
+            properties: characteristic.properties,
+            descriptors: characteristic
+                .descriptors
+                .into_iter()
+                .map(|d| d.uuid)
+                .collect(),
+        });
+    }
+    Ok(result)
 }
